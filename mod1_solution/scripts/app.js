e8434fe1a1820578b834da9937b0df76073d1a20
Pawan Singh <psingh0912@gmail.com>
2017-02-14 15:41:31 +0530

    Added colors to message

--- mod1_solution/scripts/app.js
+++ mod1_solution/scripts/app.js
@@ -6,27 +6,28 @@
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
 
-    $scope.dishes = "a, b, c";
-
     $scope.numDishes = function () {
-      return $scope.dishes.split(",").length;
-    };
-
-    $scope.checkDishes = function () {
-      // console.log($scope.numDishes() < 4);
-
-      if ($scope.numDishes() == 0) {
-        $scope.message = "Please enter some data!";
-      } else if ($scope.numDishes() < 4) {
-        $scope.message = "Enjoy!";
-      } else {
-        $scope.message = "Too much!";
+      // $scope.dishes will be undefined if empty
+      if ($scope.dishes == undefined){ return 0}
+      else {
+        return $scope.dishes.split(',').filter(function(e){return e}).length;
+        // Empty elements in the array (, ,) or ("","") will not be counted
       }
     };
 
+    $scope.checkDishes = function () {
 
-    // console.log($scope.message);
-
+      if ($scope.numDishes() == 0) {
+        $scope.message = "Please enter some data!";
+        $scope.fontcolor = "#f00"
+      } else if ($scope.numDishes() < 4) {
+        $scope.message = "Enjoy!";
+        $scope.fontcolor = "#0f0"
+      } else {
+        $scope.message = "Too much!";
+        $scope.fontcolor = "#0f0"
+      }
+    };
 
   }
 }());
